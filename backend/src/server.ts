ab63c923f3f062eeefc63cc59c6ffb89c977429b
Brandon Cook <bcook2289@gmail.com>
2025-12-08 11:04:14 +0900

    feat authService complete

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,21 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import dotenv from "dotenv";
+// import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
+
 const app = express();
-const prisma = new PrismaClient();
+const PORT = process.env.PORT || 4000;
+// const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log("Origin: ", req.headers.origin)
-    res.json({message: "Backend running" });
+app.get("/", (_req, res) => {
+    res.send("Running");
 });
 
-app.get("/users", async (_req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-});
-
-const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
 })
---
+++ backend/src/service/authService.ts
@@ -0,0 +1,83 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type userData = {
+    email: string;
+    password: string;
+}
+
+// Check if user inputted has correct information
+const findUniqueUser = async (email: string, password?: string): Promise<User> => {
+    const user = await prisma.user.findUnique({where: {email}});
+
+    if(!user) {
+        throw new Error("User not found");
+    }
+
+    if(password) {
+    }
+
+    return user;
+};
+
+// Check if password was valid
+const isPasswordValid = async (user: User, password: string) => {
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid) {
+        throw new Error("Invalid Password");
+    }
+};
+
+// Register a new UNIQUE user
+export const registerUser = async (data: userData) => {
+    const hashPassword = await bcrypt.hash(data.password, 10);
+    const userExists = await prisma.user.findFirst({
+        where: {email: data.email}
+    })
+    if (userExists) {
+        throw new Error("User already exists");
+    }
+
+    const user = await prisma.user.create({
+        data: {
+            email: data.email,
+            password: hashPassword,
+        },
+    })
+
+    return { id: user.id, email: user.email };
+}
+
+// Login user - create new JWT token
+export const loginUser = async (data: userData) => {
+    try {
+        const user = await findUniqueUser(data.email, data.password);
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET as string,
+            {expiresIn: "1d"}
+        );
+        return {token, user};
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Logout user (cache cleared on frontend)
+export const logoutUser = async () => {
+    return {
+        message: "Logout Successful"
+    };
+}
+
+// Delete User
+export const deleteUser = async (email: string) => {
+    await findUniqueUser(email);
+    return await prisma.user.delete({
+        where: {email}
+    });
+}
